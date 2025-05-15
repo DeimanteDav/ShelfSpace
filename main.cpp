afe0897a2729b6f5fb62457e676698c99bdc8519
DeimanteDav <deimante.davi@gmail.com>
2025-05-15 19:25:17 +0300

    reset main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
-// #include "mainwindow.h"
-#include "bookdetailswindow.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
-
-    QString bookId = "XdMBTKWSfeMC"; // Replace this with a valid book ID
-
-    BookDetailsWindow *detailsWindow = new BookDetailsWindow(bookId, nullptr);
-    detailsWindow->exec();
-
+    MainWindow w;
+    w.show();
     return a.exec();
 }
